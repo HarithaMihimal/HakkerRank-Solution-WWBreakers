59a73bc9f2e7c33830fa5f8dd303e5080e5f53df
Haritha Mihimal Wilwala Arachchi <129370991+HarithaMihimal@users.noreply.github.com>
2023-07-21 22:45:35 +0530

    Create Time Conversion.cpp

---
+++ Time Conversion.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <cstring> // For strcmp
+
+std::string convertTo24HourClock(std::string time12Hour) {
+    int hours, minutes, seconds;
+    char meridian[3];
+
+    sscanf(time12Hour.c_str(), "%02d:%02d:%02d%s", &hours, &minutes, &seconds, meridian);
+
+    if (strcmp(meridian, "PM") == 0 && hours != 12) {
+        hours += 12;
+    }
+    else if (strcmp(meridian, "AM") == 0 && hours == 12) {
+        hours = 0;
+    }
+
+    char time24Hour[9];
+    sprintf(time24Hour, "%02d:%02d:%02d", hours, minutes, seconds);
+
+    return time24Hour;
+}
+
+int main() {
+    std::string time12Hour;
+    std::cin >> time12Hour;
+
+    std::string time24Hour = convertTo24HourClock(time12Hour);
+    std::cout << time24Hour << std::endl;
+
+    return 0;
+}
