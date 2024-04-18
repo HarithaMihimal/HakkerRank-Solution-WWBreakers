725deb9e92194821d36b74e9940a7a89a5476391
Haritha Mihimal Wilwala Arachchi <129370991+HarithaMihimal@users.noreply.github.com>
2024-04-18 23:39:33 +0530

    Update Time Conversion.cpp

--- Time Conversion.cpp
+++ Time Conversion.cpp
@@ -1,11 +1,14 @@
 #include <iostream>
 #include <cstring> // For strcmp
+// Function to convert 12-hour clock time to 24-hour clock time
 
 std::string convertTo24HourClock(std::string time12Hour) {
     int hours, minutes, seconds;
     char meridian[3];
+    // Parse the input string to extract hours, minutes, seconds, and meridian
 
     sscanf(time12Hour.c_str(), "%02d:%02d:%02d%s", &hours, &minutes, &seconds, meridian);
+    // Convert hours to 24-hour format if necessary
 
     if (strcmp(meridian, "PM") == 0 && hours != 12) {
         hours += 12;
@@ -13,6 +16,7 @@ std::string convertTo24HourClock(std::string time12Hour) {
     else if (strcmp(meridian, "AM") == 0 && hours == 12) {
         hours = 0;
     }
+    // Format the time in 24-hour format
 
     char time24Hour[9];
     sprintf(time24Hour, "%02d:%02d:%02d", hours, minutes, seconds);
@@ -23,8 +27,11 @@ std::string convertTo24HourClock(std::string time12Hour) {
 int main() {
     std::string time12Hour;
     std::cin >> time12Hour;
+    // Convert the 12-hour time to 24-hour format
 
     std::string time24Hour = convertTo24HourClock(time12Hour);
+        // Output the time in 24-hour format
+
     std::cout << time24Hour << std::endl;
 
     return 0;
