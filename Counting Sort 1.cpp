7f497b9a42e7f4309a533a8e4fef986b9980a509
Haritha Mihimal Wilwala Arachchi <129370991+HarithaMihimal@users.noreply.github.com>
2024-04-18 23:41:02 +0530

    Update Counting Sort 1.cpp

--- Counting Sort 1.cpp
+++ Counting Sort 1.cpp
@@ -1,8 +1,10 @@
 #include <iostream>
 #include <vector>
+// Function to perform counting sort on an array of integers
 
 std::vector<int> countingSort(std::vector<int> arr) {
     std::vector<int> freq(100, 0); // Initialize frequency array with 100 zeros
+    // Calculate the frequency of each element in the input array
 
     for (int i : arr) {
         freq[i]++; // Increment the count for each value in the array
@@ -16,13 +18,16 @@ int main() {
     std::cin >> n;
 
     std::vector<int> arr(n);
+        // Read the elements of the input array
+
     for (int i = 0; i < n; i++) {
         std::cin >> arr[i];
     }
+    // Perform counting sort on the input array
 
     std::vector<int> result = countingSort(arr);
 
-    // Print the frequency array
+    // Print the frequency array (result of counting sort)
     for (int num : result) {
         std::cout << num << " ";
     }
