18eaba32f344d6a35ab185d5243b450b8173ba11
Haritha Mihimal Wilwala Arachchi <129370991+HarithaMihimal@users.noreply.github.com>
2024-07-26 22:13:10 +0530

    Create Cats and a Mouse.py

---
+++ Cats and a Mouse.py
@@ -0,0 +1,40 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the catAndMouse function below.
+def catAndMouse(x, y, z):
+    
+    xx = abs(z - x)
+    yy = abs(z - y)
+    if xx>yy:
+        return("Cat B")
+    elif xx<yy:
+        return("Cat A")
+    else:
+        return("Mouse C")
+    
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    q = int(input())
+
+    for q_itr in range(q):
+        xyz = input().split()
+
+        x = int(xyz[0])
+
+        y = int(xyz[1])
+
+        z = int(xyz[2])
+
+        result = catAndMouse(x, y, z)
+
+        fptr.write(result + '\n')
+
+    fptr.close()
