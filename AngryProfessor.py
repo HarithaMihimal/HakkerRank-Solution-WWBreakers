a906150637a1d643266b99c62fa2bb26ab32d84b
Haritha Mihimal Wilwala Arachchi <129370991+HarithaMihimal@users.noreply.github.com>
2026-09-23 19:45:55 +0530

    Update AngryProfessor.py

--- AngryProfessor.py
+++ AngryProfessor.py
@@ -1,19 +1,8 @@
-#!/bin/python3
-
 import math
 import os
 import random
 import re
 import sys
-
-#
-# Complete the 'angryProfessor' function below.
-#
-# The function is expected to return a STRING.
-# The function accepts following parameters:
-#  1. INTEGER k
-#  2. INTEGER_ARRAY a
-#
 
 def angryProfessor(k, a):
     mines=0
